c97f630164b7329a075d019494a4d6b0cdf925ee
Yassinrby <yassinrby@outlook.com>
2021-02-26 16:43:06 +0000

    Created Category model

--- home/models.py
+++ home/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
 
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
+    category = models.CharField(max_length=25, default='default')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
-
